89021f274375f6df556a9b80eec8bcad584a3683
Daniel Rindunica <rindunicadanielnicolae@outlook.com>
2023-08-12 13:32:31 +0300

    terraform init

---
+++ infrastructure/terraform/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.77.0"
+    }
+  }
+}
+
+provider "google" {
+  project = ""
+  region  = ""
+  zone    = ""
+}
